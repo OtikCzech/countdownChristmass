c8df8992cf6492e23dfdb2eb0e42ec357de2b410
OtiK.cz <otik@otik.cz>
2022-08-04 19:40:59 +0200

    v1.0.2

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # countdownChristmass
 Countdown to Christmass. Javascript calculate the time until Christmass.
+
+
+## Usage
+
+Modify #selector to select countdown element in your page.
+
+```js
+document.addEventListener("DOMContentLoaded", function () {
+    initCountdownChristmass("#selector");
+});
+```
+
+### Treeware
+If you've recieved value we'd hugely appreciate it if you could donate to our [Ecologi forest](https://ecologi.com/otik?r=62b9a35105736ce671f890ae)
+
---
+++ index.js
@@ -0,0 +1,78 @@
+function initCountdownChristmass(selector) {
+    let now;
+    const timeInADay = 24 * 60 * 60 * 1000;
+    const timeInAHour = 60 * 60 * 1000;
+    const timeInAMinutes = 60 * 1000;
+
+    const getDateOfChristmas = function () {
+        now = new Date();
+        let thisYear = now.getFullYear();
+        let dateOfChristmas = new Date(thisYear, 11, 24).getTime();
+        if (dateOfChristmas < now - timeInADay) {
+            dateOfChristmas = new Date(thisYear + 1, 11, 24).getTime();
+        }
+
+        return dateOfChristmas;
+    };
+
+    let textCounter = function () {
+        let dateOfChristmas = getDateOfChristmas();
+
+        let daysUntilChristmas = function () {
+            return Math.abs(Math.ceil((dateOfChristmas - now) / timeInADay));
+        };
+
+        let hoursUntilChristmas = function () {
+            return Math.abs(
+                Math.ceil(((dateOfChristmas - now) % timeInADay) / timeInAHour)
+            );
+        };
+
+        let minutesUntilChristmas = function () {
+            return Math.abs(
+                Math.ceil(((dateOfChristmas - now) % timeInAHour) / timeInAMinutes)
+            );
+        };
+
+        let secondsUntilChristmas = function () {
+            return Math.abs(
+                Math.ceil(((dateOfChristmas - now) % timeInAMinutes) / 1000)
+            );
+        };
+
+        let textMore = function (cnt) {
+            if (cnt > 1) {
+                return "s";
+            }
+            return "";
+        };
+
+        let days = daysUntilChristmas();
+        let hours = hoursUntilChristmas();
+        let minutes = minutesUntilChristmas();
+        let seconds = secondsUntilChristmas();
+
+        let tDays = "day" + textMore(days);
+        let tHours = "hour" + textMore(hours);
+        let tMinutes = "minute" + textMore(minutes);
+        let tSeconds = "second" + textMore(minutes);
+
+        return `<span class="cnt-chris">
+            <span class="cnt-chris_days">${days}</span>
+            <span class="cnt-chris_days--text">${tDays}</span>
+            <span class="cnt-chris_hours">${hours}</span>
+            <span class="cnt-chris_hours--text">${tHours}</span>
+            <span class="cnt-chris_mins">${minutes}</span>
+            <span class="cnt-chris_mins-">${tMinutes}</span>
+            <span class="cnt-chris_secs">${seconds}</span>
+            <span class="cnt-chris_secs--text">${tSeconds}</span>
+        </span>`;
+    };
+
+    setInterval(function () {
+        let el = document.querySelector(selector);
+        if (el) {
+            el.innerText = textCounter();
+        }
+    }, 1000);
+}
